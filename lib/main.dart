75d8118d44243ad5dd7a8005463c87d0475296b6
djinodissingar <dissingardjino@icloud.com>
2022-03-15 15:03:03 +0100

    Intégration base de donnée et application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:projetclassb2b/Model/Utilisateur.dart';
 import 'package:projetclassb2b/register.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
