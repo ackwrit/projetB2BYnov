6bca622feaeceb87cfd8ae9187319817c10034be
djinodissingar <dissingardjino@icloud.com>
2022-03-15 17:09:55 +0100

    Mise à jour ListTile

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,8 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:projetclassb2b/Model/Utilisateur.dart';
+import 'package:projetclassb2b/functions/FirestoreHelper.dart';
 
 class dashboard extends StatefulWidget{
   @override
@@ -24,7 +27,55 @@ class dashboardState extends State<dashboard>{
 
 
   Widget bodyPage(){
-    return Text("coucou");
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirestoreHelper().fire_user.snapshots(),
+        builder: (context,snapshot){
+        if(!snapshot.hasData){
+          return CircularProgressIndicator();
+        }
+        else
+          {
+            List documents = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: documents.length,
+                itemBuilder: (context,index){
+                Utilisateur user = Utilisateur(documents[index]);
+                return ListTile(
+                  leading: (user.avatar==null)?Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage("https://voitures.com/wp-content/uploads/2017/06/Kodiaq_079.jpg.jpg"),
+                        fit: BoxFit.fill
+                      )
+                    ),
+                  ):Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: NetworkImage(user.avatar!),
+                            fit: BoxFit.fill
+                        )
+                    ),
+                  ),
+
+                  title: Text("${user.prenom} ${user.nom}"),
+                  trailing: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: (){
+                      print("modification");
+                    },
+                  ),
+                );
+                }
+            );
+          }
+        }
+    );
   }
 
 }
